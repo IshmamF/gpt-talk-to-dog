eaa437a4620d0bfff566d854ffe73f09f1683f71
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2024-03-01 13:36:21 -0500

    gpt text generation addedd to record component

--- frontend/frontend/src/components/record.js
+++ frontend/frontend/src/components/record.js
@@ -1,9 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import OpenAI from "openai";
 
 export default function Record() {
     var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
     var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
     const recognition = new SpeechRecognition();
+
+    const [transcription, setTranscription] = useState('') //states
+    const [answer, setAnswer] = useState('')
+
+    useEffect(()=>{ //fetch whenever the transcription state updates...
+        const request_url = 'http://127.0.0.1:5000/gptanswer';
+        const request_param = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ question: transcription }), // Ensure your backend expects a JSON object with a `question` field
+        };
+        fetch(request_url, request_param)
+            .then(res => res.json())
+            .then(ans => setAnswer(ans.answer))
+            .catch(err => console.error(`there was an error ${err}`))
+    }, [transcription])
+
 
     if (SpeechGrammarList) {
         var speechRecognitionList = new SpeechGrammarList();
@@ -15,8 +33,6 @@ export default function Record() {
     recognition.lang = "en-US";
     recognition.interimResults = false;
     recognition.maxAlternatives = 1;
-
-    const [transcription, setTranscription] = useState('')
 
     function handleClick() {
         recognition.start();
@@ -35,11 +51,15 @@ export default function Record() {
         console.error("Speech recognition error detected: " + event.error)
     }
 
+    console.log(transcription)
     return (
         <>
-            <button onClick={handleClick}>Record</button>
-            <br/>
-            <p>Trasncription: {transcription}</p>
+            <center>
+                <button style={{background:"pink", borderRadius:0.5}} onClick={handleClick}>Record</button>
+                <br/>
+                <p>Trasncription: {transcription}</p>
+                <p>GPT response: <br/>{answer}</p>
+            </center>
         </>
     )
 
