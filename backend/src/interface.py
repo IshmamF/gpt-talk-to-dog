f3dd1817c2bd7777444d02d002b441dc4b81b157
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2024-03-01 23:43:43 -0500

    added mdn tts

--- backend/src/interface.py
+++ backend/src/interface.py
@@ -3,7 +3,6 @@ from app import gptAnswer
 from flask_cors import CORS
 from flask import send_file
 from pathlib import Path
-from utils.toSpeech import speak
 
 app = Flask(__name__)
 CORS(app)
@@ -15,17 +14,17 @@ def landing():
 @app.route("/gptanswer", methods=['POST'])
 def getAnswer():
     request_data = request.get_json()
-    answer = gptAnswer(request_data['question'])
+    answer = 'The request was empty.' if len(request_data['question']) == 0 else gptAnswer(request_data['question'])
     return jsonify({"answer": answer})
 
-@app.route("/gptspeakanswer", methods=['GET', 'POST'])#deals with speech input
-def getSpokenAnswer():
-    if request.method == 'GET':
-        return send_file(Path.cwd()/'backend'/'src'/'audio'/'audio.mp3', mimetype='audio/mp3')
-    request_data = request.get_json()
-    answer = gptAnswer(request_data['question'])
-    speak(answer)
-    return 'successfully created file'
+# @app.route("/gptspeakanswer", methods=['GET', 'POST'])#deals with speech input
+# def getSpokenAnswer():
+#     if request.method == 'GET':
+#         return send_file(Path.cwd()/'backend'/'src'/'audio'/'audio.mp3', mimetype='audio/mp3')
+#     request_data = request.get_json()
+#     answer = gptAnswer(request_data['question'])
+#     speak(answer)
+#     return 'successfully created file'
 
 
 
--- frontend/frontend/src/components/record.js
+++ frontend/frontend/src/components/record.js
@@ -5,22 +5,30 @@ export default function Record() {
     var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
     var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
     const recognition = new SpeechRecognition();
+    const synth = window.speechSynthesis;
 
     const [transcription, setTranscription] = useState('') //states
-    const [answer, setAnswer] = useState('')
+    const [gptRes, setGptRes] = useState('')
 
     useEffect(()=>{ //fetch whenever the transcription state updates...
-        const request_url = 'http://127.0.0.1:5000/gptanswer';
-        const request_param = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ question: transcription }), // Ensure your backend expects a JSON object with a `question` field
-        };
-        fetch(request_url, request_param)
-            .then(res => res.json())
-            .then(ans => setAnswer(ans.answer))
-            .catch(err => console.error(`there was an error ${err}`))
+        // oddly enough it fetches before it needs to so this check is to ensure there is no misfire when we first call it
+        // check this over maybe it might be the post method that causes this misfire
+        // this shouldn't fire at all on the initial call since it depends on the change of the transcription state...
+        if (transcription){ 
+            const request_url = 'http://127.0.0.1:5000/gptanswer';
+            const request_param = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ question: transcription }), // Ensure your backend expects a JSON object with a `question` field
+            };
+            fetch(request_url, request_param)
+                .then(res => res.json())
+                .then(ans => setGptRes(ans.answer))
+                .catch(() => "nuttin")
+            const utterThis = new SpeechSynthesisUtterance(gptRes); //update the TTS so that it speaks the new response 
+        }
     }, [transcription])
+
 
 
     if (SpeechGrammarList) {
@@ -46,22 +54,25 @@ export default function Record() {
         const transcript = event.results[0][0].transcript
         setTranscription(transcript)
     }
+    
 
     recognition.onerror = function(event) {
         console.error("Speech recognition error detected: " + event.error)
     }
 
-    console.log(transcription)
+    const utterThis = new SpeechSynthesisUtterance(gptRes);
+    const handleSpeakClick = () => synth.speak(utterThis) //repeat what was said
+
     return (
         <>
             <center>
                 <button style={{background:"pink", borderRadius:0.5}} onClick={handleClick}>Record</button>
                 <br/>
                 <p>Trasncription: {transcription}</p>
-                <p>GPT response: <br/>{answer}</p>
+                <p>GPT response: <br/>{gptRes}</p>
+
+                {gptRes.length === 0 ? '' :<button style={{background:"yellow", borderRadius:0.5}} onClick={handleSpeakClick}>repeat</button>}
             </center>
         </>
     )
-
-
 }
